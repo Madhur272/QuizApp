72ae88d13f93fa508058f0564cb3e71a794eaab7
Madhur272 <165183645+Madhur272@users.noreply.github.com>
2024-06-11 11:31:12 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "What is the capital of Australia?",
+            "Which planet is known as the Red Planet?",
+            "Who painted the famous Mona Lisa?",
+            "Which country is known as the Land of the Rising Sun?",
+            "What is the largest mammal in the world?"
+    };
+    public static String choices [][]={
+            {"Sydney" , "Canberra" , "Melbourne" , "Perth"},
+            {"Venus" , "Mars" , "Jupiter" , "Saturn"},
+            {"Michelangelo" , "Leonardo da Vinci" , "Vincent van Gogh" , "Pablo Picasso"},
+            {"China" , "Japan" , "South Korea" , "Thailand"},
+            {"Elephant" , "Blue Whale" , "Giraffe" , "Hippopotamus"}
+    };
+    public static String correctAnswers []={
+            "Canberra" ,
+            "Mars",
+            "Leonardo da Vinci",
+            "Japan",
+            "Blue Whale"};
+}
